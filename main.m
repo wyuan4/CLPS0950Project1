6957560496c43df2db9dc30d32a1b7ab5586605c
bryanpark98 <76459485+bryanpark98@users.noreply.github.com>
2021-03-05 13:46:11 +0900

    Add files via upload

--- main.m
+++ main.m
@@ -2,7 +2,7 @@
 main_deck = repmat(1:13,1,4);
 
 %pull two middle cards from the main deck
-[pulled_card1,pulled_card2,new_deck] = middle_card(main_deck)
+[middle_card1, middle_card2, new_deck] = middle_card(main_deck)
 
 %split the deck into two from the middle (for the two players); 
 %potentially think about randomizing the hands later on in the process
@@ -17,18 +17,42 @@ p2_deck = p2_deck(randperm(length(p2_deck)));
 [p1_hand, p1_deck] = draw_hand(p1_deck);
 [p2_hand, p2_deck] = draw_hand(p2_deck);
 
+playable = true;
 
+while playable%loops until one player does not have playable cards
+   
+    %Player 1 takes their turn, and draws card
+    [p1_hand,middle_card1,middle_card2] = checkflip(p1_hand,middle_card1,middle_card2)
+    [p1_hand, p1_deck, drawable1] = fillhand(p1_hand,p1_deck)
 
-while playable
+    %Player 2 takes their turn,, and draws card
+    [p2_hand,middle_card1,middle_card2] = checkflip(p2_hand,middle_card1,middle_card2)
+    [p2_hand, p2_deck, drawable2] = fillhand(p2_hand,p2_deck)
+
+    if (~drawable1) && (~drawable2)%Replaces Middle cards with random cards. Come back to this maybe??
+        middle_card1 = randi(13);
+        middle_card2 = randi(13);
+    end
     
-%Player 1 takes their turn, and draws card
-[p1_hand,pulled_card1,pulled_card2] = checkflip(p1_hand,pulled_card1,pulled_card2)
-[p1_hand, p1_deck, drawable1] = fillhand(p1_hand,p1_deck)
-
-%Player 2 takes their turn,, and draws card
-[p2_hand,pulled_card1,pulled_card2] = checkflip(p2_hand,pulled_card1,pulled_card2)
-[p2_hand, p2_deck. drawable2] = fillhand(p2_hand,p2_deck)
-
-if drawable1 == false
+    if isempty(p1_hand) || isempty(p2_hand) %if a player's hand is empty, then end loop
+        playable = false;
+    end
+    
 end
+
+if isempty(p1_hand)
+    disp 'Player 1 is the supreme champion!!!!'
+else
+    disp 'Player 2 Wins'
+end
+
+
+
+
+
+
+
+
+
+
 
