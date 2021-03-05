b83e213a8ec6d9ad543d0ce592913e948d244b28
bryanpark98 <76459485+bryanpark98@users.noreply.github.com>
2021-03-05 13:51:38 +0900

    The basic game is finished with these files.

--- fillhand.m
+++ fillhand.m
@@ -2,10 +2,13 @@ function [new_hand, new_deck, drawable] = fillhand(hand,deck)
 %FILLHAND Fills player's hand to 5 cards from their deck
 %   Detailed explanation goes here
 num_needed = 5-length(hand);%#of cards that need to be drawn
-drawable = true
-if length(hand) == 5%Hand is full, so don't draw
-    drawable = false
+drawable = true;
+if length(hand) == 5 | isempty(deck)%Hand is full, so don't draw; deck is empty, so don't draw
+    drawable = false;
+    new_hand = hand;
+    new_deck = deck;
     return
+
 elseif length(deck)< num_needed%If deck doesn't have enough cards, take rest of the deck
     new_hand = [hand, deck];
     new_deck = [];
