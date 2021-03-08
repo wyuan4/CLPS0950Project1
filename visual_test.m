ebd69e85a3aec841c8b02efd6fabdbd2b61ee924
wyuan4 <wen_yuan@brown.edu>
2021-03-07 19:25:35 -0600

    trying to outline subplots for visuals of playing cards being put down for the game

---
+++ visual_test.m
@@ -0,0 +1,22 @@
+subplot(3,4,1);
+img = imread('\Users\yuanm\Documents\School Documents\CLPS 0950\A_clubs.png');
+imagesc(img)
+axis off
+axis image
+
+subplot(3,4,6);
+img2 = imread('\Users\yuanm\Documents\School Documents\CLPS 0950\A_diamonds.png');
+imagesc(img2)
+axis off; axis image
+
+subplot(3,4,11);
+img3 = imread('\Users\yuanm\Documents\School Documents\CLPS 0950\A_hearts.png');
+imagesc(img3)
+axis off; axis image
+
+subplot(3,4,12);
+img4 = imread('\Users\yuanm\Documents\School Documents\CLPS 0950\2_diamonds.png');
+imagesc(img4)
+axis off; axis image
+
+
