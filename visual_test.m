2a63732eac9fa2ad59492339723ec54cc21df1a6
wyuan4 <wen_yuan@brown.edu>
2021-03-08 21:55:26 -0600

    added in url's from imgur page into visual test -- 30 minutes (Wen typing, all on Gathertown)

--- visual_test.m
+++ visual_test.m
@@ -1,35 +1,34 @@
-%trying to put in visuals of the cards being played by using subplots and
-%downloaded images 
-
 subplot(3,4,1);
-img = imread('\Users\yuanm\Documents\School Documents\CLPS 0950\A_clubs.png');
-imagesc(img)
+img1 = webread('https://i.imgur.com/kD0ASl5.png');
+imagesc(img1)
 axis off
 axis image
 
-subplot(3,4,6);
-img2 = imread('\Users\yuanm\Documents\School Documents\CLPS 0950\A_diamonds.png');
-imagesc(img2)
-axis off; axis image
-
-subplot(3,4,11);
-img3 = imread('\Users\yuanm\Documents\School Documents\CLPS 0950\A_hearts.png');
-imagesc(img3)
-axis off; axis image
+img2 = webread('https://i.imgur.com/7sI5Ryi.png');
+img3 = webread('https://i.imgur.com/n9d5m1c.png');
+img4 = webread('https://i.imgur.com/31AzfTA.png');
+img5 = webread('https://i.imgur.com/bGm3bAP.png');
+img6 = webread('https://i.imgur.com/VanfFtQ.png');
+img7 = webread('https://i.imgur.com/w7QaFxU.png');
+img8 = webread('https://i.imgur.com/4n0Y1C3.png');
 
 subplot(3,4,12);
-img4 = imread('\Users\yuanm\Documents\School Documents\CLPS 0950\2_diamonds.png');
-imagesc(img4)
+img9 = imread('https://i.imgur.com/LkOqebV.png');
+imagesc(img9)
 axis off; axis image
 
+img10 = webread('https://i.imgur.com/WCJe2QX.png');
 
+subplot(3,4,6);
+url11 = 'https://i.imgur.com/AaxlUGR.png';
+img11 = webread(url11);
+imagesc(img11)
+axis off; axis image
 
+img12 = webread('https://i.imgur.com/O5Xo5Mb.png');
 
-% speedcardplot(0,0,10,5,'diamonds')
-% axis off; axis image
-% 
-% speedcardplot(10,0,10,6,'spades')
-% axis off; axis image
-% 
-% speedcardplot(0,15,10,11,'clubs')
-% axis off; axis image
+subplot(3,4,11);
+url13 = 'https://i.imgur.com/4lhzjZp.png';
+img13 = webread(url13);
+imagesc(img13)
+axis off; axis image
