694ef19998c9f579c480362e26d4b38978f8fde0
kennedywaite <kennedywaite@172.16.109.220>
2021-03-09 23:46:02 -0500

    Added the input from user to ask if want to play with jokers or not in the main. Kennedy typing, 30 minutes, all on gathertown!

--- main.m
+++ main.m
@@ -1,8 +1,13 @@
-%initiate the main vector for deck
-
+%Initiate the main vector for deck, ask if player wants to play with Jokers
+str = input('Would you like to play with Jokers? Yes/No [Answer]: ','s');
 main_deck = repmat(1:13,1,4); %this is the main deck, composed of four vectors of integers from 1 to 13
 [main_deck] = addJokers(main_deck);
 random_int = randi(2); %this determines who goes first initially
+
+
+if strcmp(str,'Yes') 
+    [main_deck] = addJokers(main_deck);
+end
 
 %next, implement the checkflip function to add a step (if statement) that checks if
 %players have jokers, putting down the joker, and then randomly assigning
